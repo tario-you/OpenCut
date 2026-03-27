8e936a759430aeb638fcae1d927c686f130c1d9a
Maze Winther <mazewinther@gmail.com>
2026-03-27 21:23:38 +0100

    chore: remove dead songs tab

--- apps/web/src/components/editor/panels/assets/views/sounds.tsx
+++ apps/web/src/components/editor/panels/assets/views/sounds.tsx
@@ -42,7 +42,6 @@ export function SoundsView() {
 				<div className="px-3 pt-4 pb-0">
 					<TabsList>
 						<TabsTrigger value="sound-effects">Sound effects</TabsTrigger>
-						<TabsTrigger value="songs">Songs</TabsTrigger>
 						<TabsTrigger value="saved">Saved</TabsTrigger>
 					</TabsList>
 				</div>
@@ -58,12 +57,6 @@ export function SoundsView() {
 					className="mt-0 flex min-h-0 flex-1 flex-col p-5 pt-0"
 				>
 					<SavedSoundsView />
-				</TabsContent>
-				<TabsContent
-					value="songs"
-					className="mt-0 flex min-h-0 flex-1 flex-col p-5 pt-0"
-				>
-					<SongsView />
 				</TabsContent>
 			</Tabs>
 		</div>
@@ -483,10 +476,6 @@ function SavedSoundsView() {
 			</div>
 		</div>
 	);
-}
-
-function SongsView() {
-	return <div>Songs</div>;
 }
 
 interface AudioItemProps {
